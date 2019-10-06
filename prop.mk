c5173859ba59912d1497ccb927976eb2c0b17882
Sundong Ahn <sundongahn@google.com>
2019-10-06 12:16:04 +0600

    msm8953-common: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014
    
    Some properties are decommonized because LCD Panels target do not
    support them

--- prop.mk
+++ prop.mk
@@ -168,6 +168,15 @@ ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
 service.qti.ims.enabled=1
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+ro.surface_flinger.use_color_management=true
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true
