697406b3eee3275add7f069c1e8a79b4f0957c6d
pawelik001 <pawelik001@gmail.com>
2019-10-07 18:19:23 +0200

    msm8953-common: VoLTE cleanup and enable VoWiFi

--- prop.mk
+++ prop.mk
@@ -156,9 +156,6 @@ persist.rild.nitz_short_ons_3=""
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PROVISIONED=1 \
-persist.dbg.volte_avail_ovr=1 \
-persist.dbg.vt_avail_ovr=1 \
-persist.dbg.wfc_avail_ovr=1 \
 persist.vendor.radio.apm_sim_not_pwdn=1 \
 persist.radio.multisim.config=dsds \
 persist.vendor.radio.custom_ecc=1 \
@@ -186,6 +183,20 @@ sys.use_fifo_ui=1
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.usb.config.extra=none
+
+# VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.dbg.ims_volte_enable=1 \
+persist.dbg.volte_avail_ovr=1 \
+persist.dbg.vt_avail_ovr=1 \
+persist.dbg.wfc_avail_ovr=1 
+
+# VoWiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.radio.rat_on=combine \
+persist.radio.data_ltd_sys_ind=1 \
+persist.radio.data_con_rprt=1 \
+persist.radio.calls.on.ims=1\
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
