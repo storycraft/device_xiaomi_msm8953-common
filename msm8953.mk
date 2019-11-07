970b48db8f335fbf725a9f241abe97073985d3e1
Zinadin Zidan <tuhinsultana075@gmail.com>
2019-11-07 18:36:13 +0600

    daisy: Update Audio From Stock MiUi

--- msm8953.mk
+++ msm8953.mk
@@ -90,8 +90,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
-    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
     libaacwrapper \
