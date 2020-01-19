04e1d03ff5c244906b4aa14120b89d7fb32ca1ae
tunasahinn <tnhnshn8181@gmail.com>
2020-01-19 19:25:24 +0000

    Revert "Merge branch 'aex-10.0' of https://github.com/daisy-development-q/device_xiaomi_msm8953-common into aex-10.0"
    This reverts commit 26621266d73c793da269f5257f6250c2b5a60719, reversing
    changes made to 2fba3e78a4a375bf06da3e5757669009434f2d6d.

--- msm8953.mk
+++ msm8953.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0 \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
@@ -143,8 +142,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.mapper@3.0 \
-    android.hardware.graphics.allocator@3.0 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     copybit.msm8953 \
