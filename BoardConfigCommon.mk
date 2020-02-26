25d809947a873a4699c7f080fd5f94718d2078d5
storycraft <storycraft@storyboard.ml>
2020-02-26 16:47:07 +0900

    daisy: add trust hal

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,8 +42,8 @@ BUILD_BROKEN_DUP_RULES := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.usbconfigfs=true
 
-# Disable selinux currently
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# selinux
+# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
