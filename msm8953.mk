47cf9f67c7833a83ebbeeb6d945d3008e08b37b3
storycraft <storycraft@storyboard.ml>
2020-03-13 16:19:01 +0900

    remove cryptfs_hw not working

--- msm8953.mk
+++ msm8953.mk
@@ -192,10 +192,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# HW Encryption
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Healthd
 PRODUCT_PACKAGES += \
     chargeonlymode
